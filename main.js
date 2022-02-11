1cfebac587de20a2582c25448985c519de292d65
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-02-11 11:36:54 -0300

    Adicionando variaveis com valores colocados pelo usuário

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
-const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minsEl = document.getElementById("mins");
-const secondsEl = document.getElementById("seconds");
+function initiateCountdown(){
 
-function countdown(){
-    const newDate = new Date();
-    const currentDate = new Date();
+    const daysEl = document.getElementById("days");
+    const hoursEl = document.getElementById("hours");
+    const minsEl = document.getElementById("mins");
+    const secondsEl = document.getElementById("seconds");
 
-    const totalSeconds = (newDate - currentDate) / 1000;
+    function countdown(){
+        const year = document.getElementById('year');
+        const yearValue = year.value;
+        const month = document.getElementById('month');
+        const monthValue = month.value;
+        const day = document.getElementById('day');
+        const dayValue = day.value;;
 
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
+        const currentDate = new Date();
+        const newDate = new Date(`${yearValue} ${monthValue} ${dayValue}`);
+        
+        const totalSeconds = (newDate - currentDate) / 1000;
 
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime(seconds);
+        const days = Math.floor(totalSeconds / 3600 / 24);
+        const hours = Math.floor(totalSeconds / 3600) % 24;
+        const mins = Math.floor(totalSeconds / 60) % 60;
+        const seconds = Math.floor(totalSeconds) % 60;
+
+        daysEl.innerHTML = days;
+        hoursEl.innerHTML = formatTime(hours);
+        minsEl.innerHTML = formatTime(mins);
+        secondsEl.innerHTML = formatTime(seconds);
+    }
+
+    function formatTime(time){
+        return time < 10 ? `0${time}` : time;
+    }
+
+    setInterval(countdown, 1000);
 }
-
-function formatTime(time){
-    return time < 10 ? `0${time}` : time;
-}
-
-setInterval(countdown, 1000);
